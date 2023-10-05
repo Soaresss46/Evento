64b85f1db8c4310010e1e7907158de0f8a90184a
Matheus <144848303+Soaresss46@users.noreply.github.com>
2023-10-05 17:25:08 -0300

    Add files via upload

---
+++ evento/scripts.js
@@ -0,0 +1,10 @@
+function linha() {
+    document.getElementById('img').src = 'https://s.alicdn.com/@sc04/kf/Adbd174827c5b4f2887c104967b60019dZ.jpg_200x200.jpg';
+}
+
+function bone() {
+    document.getElementById('img').src = 'https://comofazerartesanatos.com.br/wp-content/uploads/2022/08/crochedovini-770px-770px.jpg';
+}
+
+document.getElementById('linha').addEventListener('click', linha);
+document.getElementById('bone').addEventListener('click', bone);
